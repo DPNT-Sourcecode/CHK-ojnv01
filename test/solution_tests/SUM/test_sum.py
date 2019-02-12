0d81b7d792df87e707330284498e0e1c72cca9aa
sbx_user1059 <sbx_user1059@ip-10-128-65-216.eu-west-2.compute.internal>
2019-02-12 14:42:12 +0000

    Tue Feb 12 14:42:12 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,7 @@
-from solutions.SUM import sum_solution
+from iwoca.accelerate_runner.lib.solutions.SUM.sum_solution import compute
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
