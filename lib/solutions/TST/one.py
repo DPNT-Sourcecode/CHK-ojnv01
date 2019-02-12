0839420f1f5bd38c17b2a373b9fe6f96477807d5
sbx_user1059 <sbx_user1059@ip-10-129-69-118.eu-west-2.compute.internal>
2019-02-12 15:25:02 +0000

    Tue Feb 12 15:25:02 UTC 2019

--- lib/solutions/TST/one.py
+++ lib/solutions/TST/one.py
@@ -1,3 +1,12 @@
 
-def get():
-    return 1
+def get(items):
+    """
+    expecting a string
+
+    in theory can have any letter but only have data for A - D so won't accept anything else at the moment
+
+    """
+    if type(items) 
+
+
+
