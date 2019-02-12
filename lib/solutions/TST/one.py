0e98a17f75a5013e9b0b35849372d20d30fb33d6
sbx_user1059 <sbx_user1059@ip-10-129-69-118.eu-west-2.compute.internal>
2019-02-12 15:28:02 +0000

    Tue Feb 12 15:28:02 UTC 2019

--- lib/solutions/TST/one.py
+++ lib/solutions/TST/one.py
@@ -5,8 +5,13 @@ def get(items):
 
     in theory can have any letter but only have data for A - D so won't accept anything else at the moment
 
+    i'm going to allow upper or lower case to be entered, and convert to upper case for checking
+
     """
-    if type(items) 
+    if type(items) != str:
+        return -1
+
+    valid_letters = ['A', 'B', 'C', 'D']
 
 
 
